d9a951a5f4363e5cf9b8b78b0b754a5e192515cb
Deimos-1 <cramiajr@gmail.com>
2025-12-14 16:36:13 +0100

    Script updates

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@ cd "$(dirname "$0")"    # cd to the directory of the script to access other file
 
 ## fetch the latest version of cJSON
 echo "> Fetching the latest version of cJSON on GitHub..."
+mkdir -p include # -p flag: only if it doesnt exist
 curl -o include/cJSON.c "https://raw.githubusercontent.com/DaveGamble/cJSON/refs/heads/master/cJSON.c"
 curl -o include/cJSON.h "https://raw.githubusercontent.com/DaveGamble/cJSON/refs/heads/master/cJSON.h"
 
 ## Compile C files
 echo "> Compiling the binaries..." 
+mkdir -p bin
 gcc -Wall ./src/solve_dIdX_RK4.c ./include/cJSON.c -o ./bin/solve_dIdX_RK4  -lm
 echo "> Done !"
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ set -e                  # exits the script if an error is encountered
 cd "$(dirname "$0")"    # cd to the directory of the script to access other files by relative path
 
 # Execute the binaries for each scenario
+mkdir -p results
 for filename in ./data/scenarios/*.json; do
     echo "> Simulating: ${filename}" ; 
     ./bin/solve_dIdX_RK4 ./data/data_ding.json ${filename} ./results >> ./results/logs.txt; 
